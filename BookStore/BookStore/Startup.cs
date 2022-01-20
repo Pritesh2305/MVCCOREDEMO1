154a4be84eb9b5b5489d52333548e6df3f2645ce
Pritesh Bhavsar <bhavsarpritesh.mca@gmail.com>
2022-01-20 19:20:21 +0530

    all changes

--- BookStore/BookStore/Startup.cs
+++ BookStore/BookStore/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -44,6 +45,11 @@ namespace BookStore
 
             app.UseEndpoints(endpoints =>
             {
+                //endpoints.MapGet("/", async context =>
+                // {
+                //     await context.Response.WriteAsync(env.EnvironmentName);                     
+                // }                 
+                // );               
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
